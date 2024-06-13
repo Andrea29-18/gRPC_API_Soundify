65e88a7d88c7bf8060e4e99cbcf142f523919472
Ale_Pucheta <aavp0316@gmail.com>
2024-06-13 14:25:53 -0600

    Actualización de gRPC, quitar jpg y mejorar mp3

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const dotenv = require('dotenv');
 const fs = require('fs');
+const path = require('path');
 const PROTO_PATH = "./proto/audio.proto";
 
 // Carga la configuración del archivo .env
@@ -15,14 +16,8 @@ const audioProto = grpc.loadPackageDefinition(packageDefinition);
 const server = new grpc.Server();
 server.addService(audioProto.AudioService.service, { 
     downloadAudio: downloadAudioImpl, 
-    uploadAudio: uploadAudioImpl });
-
-/*server.addService(audioProto.ImageService.service, {
-    downloadJPEG: downloadJPEGImpl,
-    uploadJPEG: uploadJPEGImpl,
-    downloadPNG: downloadPNGImpl,
-    uploadPNG: uploadPNGImpl,
-});*/
+    uploadAudio: uploadAudioImpl 
+});
 
 // Inicia el servidor en el puerto SERVER_PORT
 server.bindAsync(`localhost:${process.env.SERVER_PORT}`, grpc.ServerCredentials.createInsecure(), () => {
@@ -43,16 +38,19 @@ function downloadAudioImpl(call) {
         console.log('\nEnvío de datos terminado.');
     });
 }
+
 // Implementación de uploadAudio
 function uploadAudioImpl(call, callback) {
     let nombreArchivo, chunk;
     let tempFilePath;
 
-    // Vamos a recibir el nombre y el stream
     call.on('data', async (DataChunkResponse) => {
         if (DataChunkResponse.nombre && DataChunkResponse.data) {
             nombreArchivo = DataChunkResponse.nombre;
-            tempFilePath = `./audio/${nombreArchivo}`;
+            if (!path.extname(nombreArchivo)) {
+                nombreArchivo += '.mp3'; // Añadir la extensión .mp3 si no está presente
+            }
+            tempFilePath = path.join(__dirname, 'audio', nombreArchivo);
             chunk = DataChunkResponse.data;
             fs.appendFileSync(tempFilePath, chunk);
             process.stdout.write('.');
@@ -60,66 +58,6 @@ function uploadAudioImpl(call, callback) {
         }
     }).on('end', function () {
         callback(null, { nombre: nombreArchivo });
-        console.log('\nEnvío de datos terminado.')
-    })
-}
-
-
-/*
-// Implementación de downloadJPEG
-function downloadJPEGImpl(call) {
-    const stream = fs.createReadStream(`./resources/${call.request.nombre}`);
-    stream.on('data', function (chunk) {
-        call.write({ data: chunk });
-    }).on('end', function () {
-        call.end();
-        console.log('Envío de datos terminado.');
+        console.log('\nEnvío de datos terminado.');
     });
 }
-// Implementación de uploadJPEG
-function uploadJPEGImpl(call, callback) {
-    let fileName;
-    let tempFilePath;
-
-    call.on('data', (DataChunkResponse) => {
-        if (DataChunkResponse.nombre) {
-            fileName = DataChunkResponse.nombre;
-            tempFilePath = `./uploads/${fileName}`;
-            console.debug(`Recibiendo el archivo JPEG: ${tempFilePath}`);
-        } else if (DataChunkResponse.data) {
-            fs.appendFileSync(tempFilePath, DataChunkResponse.data);
-        }
-    }).on('end', function () {
-        callback(null, { nombre: fileName });
-        console.log('Envío de datos terminado.');
-    });
-}
-
-// Implementación de downloadPNG
-function downloadPNGImpl(call) {
-    const stream = fs.createReadStream(`./resources/${call.request.nombre}`);
-    stream.on('data', function (chunk) {
-        call.write({ data: chunk });
-    }).on('end', function () {
-        call.end();
-        console.log('Envío de datos terminado.');
-    });
-}
-// Implementación de uploadPNG
-function uploadPNGImpl(call, callback) {
-    let fileName;
-    let tempFilePath;
-
-    call.on('data', (DataChunkResponse) => {
-        if (DataChunkResponse.nombre) {
-            fileName = DataChunkResponse.nombre;
-            tempFilePath = `./uploads/${fileName}`;
-            console.debug(`Recibiendo el archivo PNG: ${tempFilePath}`);
-        } else if (DataChunkResponse.data) {
-            fs.appendFileSync(tempFilePath, DataChunkResponse.data);
-        }
-    }).on('end', function () {
-        callback(null, { nombre: fileName });
-        console.log('Envío de datos terminado.');
-    });
-}*/
